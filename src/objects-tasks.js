dc17271d1fb66c81ebcb50213567c02937fe068f
Василевский Максим Евгеньевич <mvasilevsky@mapsoft.by>
2025-02-18 10:48:47 +0300

    init: start core-js-objects and complete mergeObject task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -32,8 +32,17 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return objects.reduce((acc, obj) => {
+    Object.entries(obj).forEach(([key, value]) => {
+      if (Object.hasOwn(acc, key)) {
+        acc[key] += value;
+      } else {
+        acc[key] = value;
+      }
+    });
+    return acc;
+  }, {});
 }
 
 /**
