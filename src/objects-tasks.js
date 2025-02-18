41992081b38a80282fd1789ccb9cb251988fc5ad
Василевский Максим Евгеньевич <mvasilevsky@mapsoft.by>
2025-02-18 16:25:56 +0300

    feat: complete sortCitiesArray

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -262,8 +262,13 @@ function fromJSON(proto, json) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => {
+    if (a.country === b.country) {
+      return a.city.localeCompare(b.city);
+    }
+    return a.country.localeCompare(b.country);
+  });
 }
 
 /**
